a56f889765e14264a8ac4a5d9a89ee3d94b17c70
Aman Parnami <amanparnami@gmail.com>
2013-04-07 16:51:54 -0400

    Implemented basic filter operation.

---
+++ Criteria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TangibleAnchoring
+{
+    public class Criteria
+    {
+        private string questionId;
+
+        public string QuestionId
+        {
+            get { return questionId; }
+            set { questionId = value; }
+        }
+
+        private string[] answerIds;
+
+        public string[] AnswerIds
+        {
+            get { return answerIds; }
+            set { answerIds = value; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="questionId">Question id to be checked</param>
+        /// <param name="csvAnswerIds">Comma separated answer ids</param>
+        public Criteria(string questionId, string csvAnswerIds)
+        {
+            this.questionId = questionId;
+
+            int numAnswerIds = csvAnswerIds.Split(',').Length;
+            answerIds = new string[numAnswerIds];
+            string[] answerIdArr = csvAnswerIds.Split(',');
+            for (int i = 0; i < numAnswerIds; i++)
+            {
+                answerIds[i] = answerIdArr[i].Trim();
+            }
+        }
+    }
+}
